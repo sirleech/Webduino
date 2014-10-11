09b0b57aabb025b8057904188bd74da4bc02ff49
corbin dunn <corbin@corbinstreehouse.com>
2014-10-11 16:40:35 -0700

    .cpp -> .ino for people who don't use Xcode (the crazies)

--- examples/Web_HelloWorld/Web_HelloWorld.ino
+++ examples/Web_HelloWorld/Web_HelloWorld.ino
@@ -1,20 +1,10 @@
-/* Web_HelloWorld.pde - very simple Webduino example */
 
+#include "Wire.h"
+#include <stdint.h>
 #include "SPI.h"
-#include "Ethernet.h"
+#include "Adafruit_CC3000.h"
 #include "WebServer.h"
 
-/* CHANGE THIS TO YOUR OWN UNIQUE VALUE.  The MAC number should be
- * different from any other devices on your network or you'll have
- * problems receiving packets. */
-static uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-
-
-/* CHANGE THIS TO MATCH YOUR HOST NETWORK.  Most home networks are in
- * the 192.168.0.XXX or 192.168.1.XXX subrange.  Pick an address
- * that's not in use and isn't going to be automatically allocated by
- * DHCP from your router. */
-static uint8_t ip[] = { 192, 168, 1, 210 };
 
 /* This creates an instance of the webserver.  By specifying a prefix
  * of "", all pages will be at the root of the server. */
@@ -26,46 +16,48 @@ WebServer webserver(PREFIX, 80);
  * server to send data back to the web browser. */
 void helloCmd(WebServer &server, WebServer::ConnectionType type, char *, bool)
 {
-  /* this line sends the standard "we're all OK" headers back to the
+
+    /* this line sends the standard "we're all OK" headers back to the
      browser */
-  server.httpSuccess();
-
-  /* if we're handling a GET or POST, we can output our data here.
+    server.httpSuccess();
+    
+    /* if we're handling a GET or POST, we can output our data here.
      For a HEAD request, we just stop after outputting headers. */
-  if (type != WebServer::HEAD)
-  {
-    /* this defines some HTML text in read-only memory aka PROGMEM.
-     * This is needed to avoid having the string copied to our limited
-     * amount of RAM. */
-    P(helloMsg) = "<h1>Hello, World!</h1>";
+    if (type != WebServer::HEAD)
+    {
+        /* this defines some HTML text in read-only memory aka PROGMEM.
+         * This is needed to avoid having the string copied to our limited
+         * amount of RAM. */
+        const char *helloMsg = "<html><body><h1>Hello, World!</h1></body></html>";
 
-    /* this is a special form of print that outputs from PROGMEM */
-    server.printP(helloMsg);
-  }
+        /* this is a special form of print that outputs from PROGMEM */
+        server.print(helloMsg);
+    }
 }
 
-void setup()
-{
-  /* initialize the Ethernet adapter */
-  Ethernet.begin(mac, ip);
-
-  /* setup our default command that will be run when the user accesses
-   * the root page on the server */
-  webserver.setDefaultCommand(&helloCmd);
-
-  /* run the same command if you try to load /index.html, a common
-   * default page name */
-  webserver.addCommand("index.html", &helloCmd);
-
-  /* start the webserver */
-  webserver.begin();
+void setup() {
+#if DEBUG
+    Serial.begin(9600);
+    Serial.println("Hello world.");
+#endif
+    
+    /* setup our default command that will be run when the user accesses
+     * the root page on the server */
+    webserver.setDefaultCommand(&helloCmd);
+    
+    /* run the same command if you try to load /index.html, a common
+     * default page name */
+    webserver.addCommand("index.html", &helloCmd);
+    
+    /* start the webserver */
+    webserver.begin();
 }
 
 void loop()
 {
-  char buff[64];
-  int len = 64;
-
-  /* process incoming connections one at a time forever */
-  webserver.processConnection(buff, &len);
+    char buff[64];
+    int len = 64;
+    
+    /* process incoming connections one at a time forever */
+    webserver.processConnection(buff, &len);
 }
